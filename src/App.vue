b802c4aee9fe62a591a2e0f8eafe9686ef3c320a
Felipe Pino <felipepinoredes@gmail.com>
2020-06-08 18:07:16 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form-one></form-one>
+    
+  </div>
+</template>
+
+<script>
+import formOne from './components/formOne';
+import forTwo from './components/formTwo.vue';
+
+export default {
+  components:{
+    'form-one':formOne,
+    'form-two':formTwo,
+  },
+  data(){
+    return {
+     
+    }
+  },
+  methods:{
+    
+    }
+  }
+</script>
+<style>
+
+</style>
